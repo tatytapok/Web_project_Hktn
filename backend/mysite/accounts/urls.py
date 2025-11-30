e40ea148ae1c92a553281e0fb867aba3649a9d18
Rat01010000 <mb4bl4@mail.ru>
2025-11-30 12:10:01 +0300

    New changes static ()

--- backend/mysite/accounts/urls.py
+++ backend/mysite/accounts/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', LogoutView.as_view(next_page='/accounts/login/'), name='logout'),
     path('dashboard/', views.teacher_dashboard, name='teacher_dashboard'),
-]+ staticfiles_urlpatterns(
+]+ staticfiles_urlpatterns()
